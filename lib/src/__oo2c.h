a525558a4faf427fe74bea5f7edda19d4f92f959
mva <mva>
2002-12-19 03:18:22 +0000

    The rest of the UCS4CHAR implementation.  LONGCHAR tests have been extended to cover UCS4CHAR as well.

--- lib/src/__oo2c.h
+++ lib/src/__oo2c.h
@@ -1,4 +1,4 @@
-/*      $Id: __oo2c.h,v 1.38 2002/12/18 11:13:59 mva Exp $        */
+/*      $Id: __oo2c.h,v 1.39 2002/12/19 03:18:23 mva Exp $        */
 /*  Run-time system for C back-ends of OOC2
     Copyright (C) 2001, 2002  Michael van Acken
 
@@ -66,8 +66,11 @@
 
 /* COPY(s,d) */
 extern void _copy_8(const OOC_CHAR8* src, OOC_CHAR8* dest, OOC_LEN max_len);
-extern void _copy_16(const OOC_CHAR16* src, OOC_CHAR16* dest, OOC_LEN max_len);
 extern void _copy_8to16(const OOC_CHAR8* src, OOC_CHAR16* dest, OOC_LEN max_len);
+extern void _copy_8to32(const OOC_CHAR8* src, OOC_CHAR32* dest, OOC_LEN max_len);
+extern void _copy_16(const OOC_CHAR16* src, OOC_CHAR16* dest, OOC_LEN max_len);
+extern void _copy_16to32(const OOC_CHAR16* src, OOC_CHAR32* dest, OOC_LEN max_len);
+extern void _copy_32(const OOC_CHAR32* src, OOC_CHAR32* dest, OOC_LEN max_len);
 
 /* copy record or array value */
 #define _copy_block(_src,_dest,_len) \
@@ -80,6 +83,7 @@ extern void _copy_8to16(const OOC_CHAR8* src, OOC_CHAR16* dest, OOC_LEN max_len)
 /* string compare */
 extern OOC_INT32 _cmp8(const OOC_CHAR8* l, const OOC_CHAR8* r);
 extern OOC_INT32 _cmp16(const OOC_CHAR16* l, const OOC_CHAR16* r);
+extern OOC_INT32 _cmp32(const OOC_CHAR32* l, const OOC_CHAR32* r);
 
 /* range of set values */
 #define _bit_range(_from,_to) (_from > _to) ? (OOC_UINT32)0 : \
